9ba0006b0c178c4008bae61500f951d5502cf9fa
Fred Sladkey <fsladkey@gmail.com>
2017-02-08 10:38:38 -0500

    Update start script

--- node_server/util.js
+++ node_server/util.js
@@ -1,14 +1,14 @@
 const http = require('http');
 
-function get(url) {
-  return new Promise((resolve) => {
-    function handleResponse(response) {
-      let str = '';
-      response.on('data', (chunk) => str += chunk);
-      response.on('end', () => resolve(JSON.parse(str)));
-    }
-    http.request(url, handleResponse).end();
-  });
+module.exports = {
+  get(url) {
+    return new Promise((resolve) => {
+      function handleResponse(response) {
+        let str = '';
+        response.on('data', (chunk) => str += chunk);
+        response.on('end', () => resolve(JSON.parse(str)));
+      }
+      http.request(url, handleResponse).end();
+    });
+  }
 }
-
-module.exports.get = get;
--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
-npm install
+trap "kill 0" SIGINT
 
-if [netstat -anp tcp | grep -iq 8888 && netstat -anp tcp | grep -iq 1337]; then
+if [[ $( lsof -i tcp:3000) = *node* || $( lsof -i tcp:8080) = *Python* ]]; then
   echo "Error: Application ports are already in use"
 else
-  set -o errexit
+  npm install --prefix ./node_server
   node node_server/server.js & python python_server/server.py
 fi
