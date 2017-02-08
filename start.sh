45d6e0db6a610afd7a34c83a41f47cf558931aa0
Fred Sladkey <fsladkey@gmail.com>
2017-02-08 10:45:23 -0500

    Add moment to package.json

--- start.sh
+++ start.sh
@@ -3,6 +3,8 @@ trap "kill 0" SIGINT
 if [[ $( lsof -i tcp:3000) = *node* || $( lsof -i tcp:8080) = *Python* ]]; then
   echo "Error: Application ports are already in use"
 else
-  npm install --prefix ./node_server
+  if [ ! -d "./node_server/node_modules" ]; then
+    npm install --prefix ./node_server
+  fi
   node node_server/server.js & python python_server/server.py
 fi
